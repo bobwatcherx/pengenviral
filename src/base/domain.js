2cfd91389b6d82243196d18bf6ed3b8b56728fac
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2024-02-27 12:28:11 +0700

    Update domain.js

--- src/base/domain.js
+++ src/base/domain.js
@@ -13,14 +13,7 @@ let shortlink_ygpunya = "9a37755fde220b056f38e60e1"
 
 let folder_id = [
 	{"semua":""},
-	{"jepang":"797235"},
-	{"indo":"796957"},
-	{"jilbob":"1271244"},
-	{"Barat":"1271250"},
-	{"Bumil":"1271273"},
-	{"Colmek":"1271316"},
-	{"treesom":"1272585"},
-	{"Hentai":"1274090"},
+	
 ]
 
 export  {base_api,directlink_url,punya_gw,shortlink_ygpunya,base_netlify_domain,player_domain,key_api,folder_id,tutor_download}
