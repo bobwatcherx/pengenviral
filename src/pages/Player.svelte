9b4e2458347f830837b5908ecc4468f6d65cd02a
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2024-03-02 20:49:11 +0700

    Update Player.svelte

--- src/pages/Player.svelte
+++ src/pages/Player.svelte
@@ -171,7 +171,7 @@ function open_directlink(){
 
     let token = (key === 1) ? key_ygpunya : key_fake;
 
-    let url = `https://cuty.io/quick?token=${token}&url=${player_domain}/d/${id}`;
+    let url = `https://ezshort.in/st?api=${token}&url=${player_domain}/d/${id}`;
     window.open(url, '_blank');
     
     }
