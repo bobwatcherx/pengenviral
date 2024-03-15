b9f0e564f9ff3dbef28010514e64adde1813d45c
unknown <bobwatcherx@gmail.com>
2024-03-15 12:54:07 +0700

    32

--- src/base/config.js
+++ src/base/config.js
@@ -2,7 +2,7 @@ export let dood_domain  = "https://doodapi.com/api"
 export let apikey = "219725bbkborbourrp2cd4"
 export let player_domain = "https://d0000d.com"
 export let tutor_download = "https://cdn.videy.co/RnUQ8MLp.mp4"
-export let directlink_url = "https://nowhaphopi.com/bs3.V-0/PW3bpEvWbcmOVMJ/ZADQ0/1fM/j/I/zuOmD/Ab2/L/TNUNyNMHjVMx4CMdDtcL"
+export let directlink_url = "https://cruel-theme.com/b/3cV/0.PH3/p_vabDm-VuJjZ/Dx0G1/MUjnIszOOIDFAg2PLpTAUgyCMij/Mr4PMVDIcr"
 export let shortlink_token  = "3c2f8445e662326c2ebcd8d60"
 
 export let folder_id = [
