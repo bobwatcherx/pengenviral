582d9fa488d8b3fd0e877a6abcd7c336d33def77
unknown <bobwatcherx@gmail.com>
2024-04-02 15:24:32 +0700

    32

--- src/base/config.js
+++ src/base/config.js
@@ -1,5 +1,5 @@
 export let dood_domain  = "https://doodapi.com/api"
-export let apikey = "219725bbkborbourrp2cd4"
+export let apikey = "360242hafn0wsirsn83i57"
 export let player_domain = "https://d0000d.com"
 export let tutor_download = "https://cdn.videy.co/RnUQ8MLp.mp4"
 export let directlink_url = "https://biodegradablegrand.com/bI3.Vo0jPF3rpGvGb_mhV/J/ZzDj0t1FMAjeIBzVOFDNA/2wL-TIULyuMZjsMt4qMYDhcA"
@@ -8,13 +8,6 @@ export let shortlink_token  = "3c2f8445e662326c2ebcd8d60"
 
 
 export let folder_id = [
-	{"jepang":"797235"},
-	{"indo":"796957"},
-	{"jilbob":"1271244"},
-	{"Barat":"1271250"},
-	{"Bumil":"1271273"},
-	{"Colmek":"1271316"},
-	{"treesom":"1272585"},
-	{"Annal":"1292444"},
-	{"Hentai":"1274090"},
+	{"semua":""},
+	
 ]
